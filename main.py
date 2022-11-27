81771530b26294f8f184b3253c8fe21cab07d4d7
everythinghazel <112515847+everythinghazel@users.noreply.github.com>
2022-11-27 01:24:07 +0000

    7-4

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ numbers = [5, 20, 30, 30, 50]
 delval = int(input('Enter the deletion value: '))
 
 # ******************************
-# Make your Code
+numbers.remove(delval)
 # ******************************
 
 print (numbers)
